c24a0bc44122eebb0e69a8f1cb65e2b18f02d340
Staehler, Michael <michael.staehler@opitz-consulting.com>
2015-09-29 20:39:44 +0200

    added comment

--- src/main/java/de/fred4jupiter/spring/boot/jsf/SessionReplicationAwareScopeMetadataResolver.java
+++ src/main/java/de/fred4jupiter/spring/boot/jsf/SessionReplicationAwareScopeMetadataResolver.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.ScopeMetadata;
 import org.springframework.context.annotation.ScopeMetadataResolver;
 import org.springframework.context.annotation.ScopedProxyMode;
 
+/**
+ * With this class you can configure to be all session and view scoped beans to be proxied with a target based proxy.
+ * The generated proxy implements Serializable and handles the spring bean (de)serialization for session replication.
+ *
+ * <p>Enable this class in ComponentScan annotation</p>
+ */
 public class SessionReplicationAwareScopeMetadataResolver extends AnnotationScopeMetadataResolver implements ScopeMetadataResolver {
 
     @Override
