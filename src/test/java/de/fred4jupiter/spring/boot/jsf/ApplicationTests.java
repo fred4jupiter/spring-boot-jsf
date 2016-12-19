f2cd6ba07ad62a3b98d3c74463badceb763539ca
Staehler, Michael <michael.staehler@opitz-consulting.com>
2016-12-19 18:04:42 +0100

    update test config

--- src/test/java/de/fred4jupiter/spring/boot/jsf/ApplicationTests.java
+++ src/test/java/de/fred4jupiter/spring/boot/jsf/ApplicationTests.java
@@ -1,17 +1,17 @@
 package de.fred4jupiter.spring.boot.jsf;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 @WebAppConfiguration
 public class ApplicationTests {
 
